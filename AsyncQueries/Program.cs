3887d8eb5dbd56cdec21b2cec44f5c65906b7365
Nesterchuk Volodymyr <ampersant@ukr.net>
2026-01-03 19:24:19 +0100

    Added async queries and change tracking

---
+++ AsyncQueries/Program.cs
@@ -0,0 +1,73 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncQueries
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+           _ = GetAllProducts().ContinueWith(async result =>
+            {
+                foreach (var product in result.Result)
+                {
+                    Console.WriteLine(product.Name);
+                }
+
+                await BulkUpdateAsync();
+            });
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.ReadKey();
+        }
+
+
+        public static async Task<List<Product>> GetAllProducts()
+        {
+            using var context = new StoreDbContext();
+
+            return await context.Products.ToListAsync();
+        }
+
+        public static async Task BulkUpdateAsync()
+        {
+            using (var context = new StoreDbContext())
+            {
+                var productsForUpdate = await context.Products.Where(p => p.Price > 1000).ToListAsync();
+
+                foreach (var product in productsForUpdate)
+                {
+                    product.Price *= 1.1m;
+                }
+
+                context.SaveChanges();
+
+                // FAST 
+                await context.Products.Where(p => p.Price > 1000)
+                              .ExecuteUpdateAsync(setters =>
+                                             setters.SetProperty(
+                                                     p => p.Price,
+                                                     p => p.Price * 1.1m
+                                             )
+                                            .SetProperty(p => p.Description, p => "EMPTY"));
+
+
+                /* UPDATE PRODUCT 
+                   SET Price = Price * 0.1,
+                       Description = 'EMPTY'
+                   WHERE Id IN (SELECT ID FROM PRODUCTS WHERE Price > 1000) */
+
+
+                await context.Products.Where(p => p.Price < 1000)
+                             .ExecuteDeleteAsync();
+
+                /* DELETE PRODUCTS WHERE Price < 1000 */
+            }
+        }
+    }
+}
---
+++ ChangeTracking/Program.cs
@@ -0,0 +1,87 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChangeTracking
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            using var context = new StoreDbContext();
+
+            var product = new Product
+            {
+                Name = "New Product",
+                Price = 99.99m,
+                Description = "This is a new product"
+            };
+
+
+            Console.WriteLine(context.Entry(product).State);
+
+            context.Products.Add(product);
+
+            Console.WriteLine(context.Entry(product).State);
+
+            context.SaveChanges();
+
+            Console.WriteLine(context.Entry(product).State);
+
+            var existingProduct = context.Products.FirstOrDefault(p => p.Name == "New Product");
+
+            Console.WriteLine(context.Entry(existingProduct).State);
+
+            existingProduct.Price = 120.99m;
+
+            Console.WriteLine(context.Entry(existingProduct).State);
+
+            context.SaveChanges();
+
+            Console.WriteLine(context.Entry(existingProduct).State);
+
+            context.Products.Remove(existingProduct);
+
+            Console.WriteLine(context.Entry(existingProduct).State);
+
+            context.SaveChanges();
+
+            Console.WriteLine(context.Entry(existingProduct).State);
+
+            existingProduct.Price = 59.99m;
+
+            Console.WriteLine(context.Entry(existingProduct).State);
+
+
+            Console.WriteLine(new String('-', 50));
+
+            var result = context.Products.AsNoTracking().ToList();
+
+            foreach (var prod in result)
+            {
+                prod.Price += 10;
+
+                Console.WriteLine(context.Entry(prod).State);
+            }
+
+            foreach (var prod in result)
+            {
+                if (prod.Price > 100)
+                {
+                    // context.Attach(prod);
+                    //   Console.WriteLine(context.Entry(prod).State);
+                    prod.Price += 10;
+                    prod.Description = "3424325345";
+                    Console.WriteLine(context.Entry(prod).State);
+
+                    // UPDATE PRODUCTS SET PRICE = PRICE + 10 WHERE Id = 12
+                    context.Entry(prod).Property(p => p.Price).IsModified = true;
+                    Console.WriteLine(context.Entry(prod).State);
+                }
+
+                Console.WriteLine(context.Entry(prod).State);
+            }
+
+            Console.WriteLine(context.ChangeTracker.DebugView.LongView.ToString());
+        }
+    }
+}
